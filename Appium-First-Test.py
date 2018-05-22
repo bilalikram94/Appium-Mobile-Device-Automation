0964ea7a5261aa5ef268acc4cc5a77f2a141c6c2
bilalikram94 <bilalikram94@gmail.com>
2018-05-22 16:53:06 +0500

    working

--- Appium-First-Test.py
+++ Appium-First-Test.py
@@ -1,8 +1,10 @@
 import unittest
+import os
 from appium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.ui import WebDriverWait
+import time
 
 
 class Today_Today(unittest.TestCase):
@@ -21,13 +23,16 @@ class Today_Today(unittest.TestCase):
         self.dc['appActivity'] = '.MainActivity'
         self.dc['platformName'] = 'android'
         self.dc['version'] = '8.0.0'
-        self.dc
-        self.driver = webdriver.Remote('http://localhost:5037/wd/hub', self.dc)
+        self.dc['deviceName'] = 'RNE-L21'
+        self.driver = webdriver.Remote('http://127.0.0.1:4726/wd/hub', self.dc)
+        print("jsu testing", self.driver)
+        time.sleep(3)
 
     def testToday_Today(self):
         self.driver.switch_to.context("NATIVE_APP")
-        WebDriverWait(self.driver, 10).until(
-            expected_conditions.presence_of_element_located((By.XPATH, "//*[@text='Categories']")))
+        time.sleep(5)
+        self.driver.find_elements_by_xpath("//*[@text='SHOP NOW']").click()
+        WebDriverWait(self.driver, 10).until(expected_conditions.presence_of_element_located((By.XPATH, "//*[@text='Categories']")))
         self.driver.find_element_by_xpath("//*[@x='846']").click()
         self.driver.find_element_by_xpath("//*[@x='48']").click()
         self.driver.find_element_by_xpath("//*[@x='48']").click()
